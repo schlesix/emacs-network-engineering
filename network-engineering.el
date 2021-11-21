4dce1a4b3a2e63fe0fb9146a989afe60738ac1aa
Thomas Schlesinger <schlesix@gmail.com>
2021-11-21 17:55:06 +0100

    Letzter Sync Mac Mini: 2021-11-21 17:55:06

--- network-engineering.el
+++ network-engineering.el
@@ -1,5 +1,4 @@
-;; This buffer is for text that is not saved, and for Lisp evaluation.
-;; To create a file, visit it with C-x C-f and enter text in its buffer.
+;;;; This code is not ready for use, I'm a LISP rookie, tinkering around at the moment
 
 
 ;(require 'cl)
@@ -45,6 +44,111 @@
 	 )
 	)))
 
+(defun ddn2cidr (ddn)
+  (interactive "DDN: ")
+  (setq cidr "/0")
+  (cond ((string= ddn "128.0.0.0")
+	 (setq cidr "/1"))
+	((string= ddn "192.0.0.0")
+	 (setq cidr "/2"))
+	((string= ddn "224.0.0.0")
+	 (setq cidr "/3"))
+	((string= ddn " 240.0.0.0")
+	 (setq cidr "/4"))
+	 ((string= ddn "255.255.255.255")
+	 (setq cidr "/32"))
+	 ((string= ddn "255.255.255.255")
+	 (setq cidr "/32"))
+	 ((string= ddn "255.255.255.255")
+	 (setq cidr "/32"))
+	 ((string= ddn "255.255.255.255")
+	 (setq cidr "/32"))
+	 ((string= ddn "255.255.255.255")
+	 (setq cidr "/32"))
+	 ((string= ddn "255.255.255.255")
+	 (setq cidr "/32"))
+	 ((string= ddn "255.255.255.255")
+	 (setq cidr "/32"))
+	 ((string= ddn "255.255.255.255")
+	 (setq cidr "/32"))
+	 ((string= ddn "255.255.255.255")
+	 (setq cidr "/32"))
+	 ((string= ddn "255.255.255.255")
+	 (setq cidr "/32"))
+	 ((string= ddn "255.255.255.255")
+	 (setq cidr "/32"))
+	 ((string= ddn "255.255.255.255")
+	 (setq cidr "/32"))
+	 ((string= ddn "255.255.255.255")
+	 (setq cidr "/32"))
+	 ((string= ddn "255.255.255.255")
+	 (setq cidr "/32"))
+	 ((string= ddn "255.255.255.255")
+	 (setq cidr "/32"))
+	 ((string= ddn "255.255.255.255")
+	 (setq cidr "/32"))
+	 ((string= ddn "255.255.255.255")
+	 (setq cidr "/32"))
+	 ((string= ddn "255.255.255.255")
+	 (setq cidr "/32"))
+	 ((string= ddn "255.255.255.255")
+	 (setq cidr "/32"))
+	 ((string= ddn "255.255.255.255")
+	 (setq cidr "/32"))
+	 ((string= ddn "255.255.255.255")
+	 (setq cidr "/32"))
+	 ((string= ddn "255.255.255.255")
+	 (setq cidr "/32"))
+	 ((string= ddn "255.255.255.255")
+	 (setq cidr "/32"))
+	 ((string= ddn "255.255.255.255")
+	 (setq cidr "/32"))
+	 ((string= ddn "255.255.255.255")
+	 (setq cidr "/32"))
+	 ((string= ddn "255.255.255.255")
+	 (setq cidr "/32"))
+	 ((string= ddn "255.255.255.255")
+	 (setq cidr "/32"))
+	 ((string= ddn "255.255.255.255")
+	 (setq cidr "/32"))
+		
+
+	((string= ddn "255.255.255.255")
+	 (setq cidr "/32"))
+;        
+;       192.0.0.0 
+;      224.0.0.0 
+;       240.0.0.0 
+;       248.0.0.0 
+;       252.0.0.0 
+;       254.0.0.0 
+;       255.0.0.0 
+;     255.128.0.0 
+;     255.224.0.0 
+;     255.240.0.0 
+;     255.248.0.0 
+;     255.252.0.0 
+;     255.254.0.0 
+;     255.255.0.0 
+;   255.255.128.0 
+;   255.255.192.0 
+;   255.255.224.0 
+;   255.255.240.0 
+;   255.255.248.0 
+;   255.255.252.0 
+;   255.255.254.0 
+;   255.255.255.0 
+; 255.255.255.128 
+; 255.255.255.192 
+; 255.255.255.224 
+; 255.255.255.240 
+; 255.255.255.248 
+; 255.255.255.252 
+; 255.255.255.254 
+; 255.255.255.255 
+	)
+  (message cidr))
+;  (message ddn)
 
 ;;
 
